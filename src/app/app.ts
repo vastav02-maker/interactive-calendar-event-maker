b11255364724e559b85ae0d132c8753c38428257
meghrajjaiswal04-ai <meghrajjaiswal04@gmail.com>
2026-05-25 22:08:56 +0530

    week 4 event creation feature added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,47 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EventService } from './services/event';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class AppComponent {
 
-  events: any[] = [];
+  events: any[] = [
+    {
+      title: 'Team Meeting',
+      date: '2026-05-25'
+    },
+    {
+      title: 'Project Submission',
+      date: '2026-05-27'
+    }
+  ];
 
-  constructor(private eventService: EventService) {
-    this.events = this.eventService.getEvents();
+  eventForm;
+
+  constructor(private fb: FormBuilder) {
+
+    this.eventForm = this.fb.group({
+      title: ['', Validators.required],
+      date: ['', Validators.required]
+    });
+
+  }
+
+  addEvent() {
+
+    if (this.eventForm.valid) {
+
+      this.events.push(this.eventForm.value);
+
+      this.eventForm.reset();
+
+    }
+
   }
 }
